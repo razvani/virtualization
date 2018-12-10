6c3c2a46e21797497a54c9fd1e33e20692e3bfab
razvani <45756720+razvani@users.noreply.github.com>
2018-12-10 13:13:30 +0200

    test
    ed

---
+++ PowerShell/GetStatsVM.ps1
@@ -0,0 +1,6 @@
+﻿Get-VM | Where {$_.PowerState -eq "PoweredOn"} | Select Name, NumCpu, MemoryMB, `
+@{N="CPU Usage (Max), %" ; E={[Math]::Round((($_ | Get-Stat -Stat cpu.usage.average -Start (Get-Date).AddDays(-7) -Finish (Get-Date) | Measure-Object Value -Maximum).Maximum),2)}}, `
+@{N="Memory Usage (Max), %" ; E={[Math]::Round((($_ | Get-Stat -Stat mem.usage.average -Start (Get-Date).AddDays(-7) -Finish (Get-Date) | Measure-Object Value -Maximum).Maximum),2)}} |`
+Export-Csv -Path "./ECCBEHAVC02.eccmgmt.local.csv"
+
+
---
+++ PowerShell/GetVM_info.ps1
@@ -0,0 +1,9 @@
+﻿$ImportFile = '.\ServerList.csv'
+
+
+Import-csv $ImportFile | ForEach-Object {
+
+$Name = $_.Hostname
+Get-VM | Where {$_.Name -contains $Name} | Select Name, VMHost
+
+}
