53981fdec8b8e0f47ecbed982842131746cc8ab2
Jeroen Lambrichts <36880952+cgkjlambrichts@users.noreply.github.com>
2020-06-24 14:33:55 +0200

    Add files via upload
    When a VM is migrated from a 3rd party towards our infrastructure, there are some additional parameters required. The document can be found here: 
    
    https://portal.cegeka.com/be/div6/BU/Delivery/virtualization/_layouts/15/WopiFrame.aspx?sourcedoc=/be/div6/BU/Delivery/virtualization/Virtualization%20Library/VIRT%20-%20Onboarding%20VM%20requirements.docx&action=default
    
    The parameters that I have scripted: 
    
    - ctkEnabled
    - evcCompatibilityMode
    - isolation.bios.bbs.disable
    - isolation.device.connectable.disable
    - isolation.device.edit.disable
    - isolation.ghi.host.shellAction.disable
    - isolation.tools.diskShrink.disable
    - isolation.tools.diskWiper.disable
    - isolation.tools.dispTopoRequest.disable
    - isolation.tools.getCreds.disable
    - isolation.tools.ghi.autologon.disable
    - isolation.tools.ghi.launchmenu.change
    - isolation.tools.ghi.protocolhandler.info.disable
    - isolation.tools.ghi.trayicon.disable
    - isolation.tools.guestDnDVersionSet.disable
    - isolation.tools.hgfsServerSet.disable
    - isolation.tools.memSchedFakeSampleStats.disable
    - isolation.tools.trashFolderState.disable
    - isolation.tools.unity.disable
    - isolation.tools.unity.push.update.disable
    - isolation.tools.unity.taskbar.disable
    - isolation.tools.unity.windowContents.disable
    - isolation.tools.unityActive.disable
    - isolation.tools.unityInterlockOperation.disable
    - isolation.tools.vixMessage.disable
    - isolation.tools.vmxDnDVersionGet.disable
    - log.keepOld
    - log.rotateSize
    - mks.enable3d
    - tools.guestlib.enableHostInfo

---
+++ PowerCLI/AdvancedParameters.ps1
@@ -0,0 +1,247 @@
+﻿### VM Information ###
+
+$VMName = Read-Host "VMName e.g. CI000000-VMName"
+
+### Advanced Parameters ###
+
+$DesiredConfig = @(
+
+    @{
+
+        Name = 'ctkEnabled'
+        Value = 'TRUE'
+
+        },
+
+    @{
+
+        Name = 'evcCompatibilityMode'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.bios.bbs.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.device.connectable.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.device.edit.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.ghi.host.shellAction.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.diskShrink.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.dispTopoRequest.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.getCreds.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.ghi.autologon.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.ghi.launchmenu.change'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.ghi.protocolhandler.info.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.ghi.trayicon.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.guestDnDVersionSet.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.hgfsServerSet.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.memSchedFakeSampleStats.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.trashFolderState.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.unity.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.unity.push.update.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.unity.taskbar.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.unity.windowContents.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.unityActive.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.unityInterlockOperation.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.vixMessage.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'isolation.tools.vmxDnDVersionGet.disable'
+        Value = 'TRUE'
+
+    },
+
+    @{
+
+        Name = 'log.keepOld'
+        Value = '10'
+
+    },
+
+    @{
+
+        Name = 'log.rotateSize'
+        Value = '1024000'
+
+    },
+
+    @{
+
+        Name = 'mks.enable3d'
+        Value = 'FALSE'
+
+    },
+
+    @{
+
+        Name = 'tools.guestlib.enableHostInfo'
+        Value = 'FALSE'
+
+    }
+)
+
+ForEach($VM in $VMName) {
+
+    $DesiredConfig | %{
+
+        $setting = Get-AdvancedSetting -Entity $vm -Name $_.Name
+
+        if($setting){
+
+            if($setting.Value -eq $_.Value){
+
+                Write-Output "Setting $($_.Name) present and set correctly"
+
+            }
+
+            else{
+
+                Write-Output "Setting $($_.Name) present but not set correctly" 
+
+                Set-AdvancedSetting -AdvancedSetting $setting -Value $_.Value -Confirm:$false
+
+            }
+
+        }
+
+        else{
+
+            Write-Output "Setting $($_.Name) not present."
+
+            New-AdvancedSetting -Name $_.Name -Value $_.Value -Entity $vm -Confirm:$false
+
+        }
+
+    }
+
+}
