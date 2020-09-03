0a3a67c10f88657959194855898b58d4cc07e4e5
RazvanI <razvan.irimia@cegeaka.com>
2020-09-03 15:57:29 +0300

    updated

--- PowerCLI/Crelan DR/01_Create_CSV.ps1
+++ PowerCLI/Crelan DR/01_Create_CSV.ps1
@@ -10,7 +10,6 @@
    Another example of how to use this cmdlet
 #>
 
-
 # vCenter FQDN
 $vCenter = "hn1584.crelan.be"
 
@@ -19,38 +18,30 @@ $logfile = "DRP_vm_list.csv"
 
 # Array of VIRP CI's. You can add as many as you want separated by coma.
 # sample: $Virps= @("HI56789","")
-$Virps= @("CI00048635")
+$Virps = @("CI00048635")
 
 # Set the working folder the same as where the current script is located
-set-location $PSScriptRoot
+Set-Location $PSScriptRoot
 
 
-Function Connect2vCenter
-{
-
+Function Connect2vCenter {
     Param 
-        (
-            [string]$vCenter
-        )
-
-                # Connecting to vCenter and check for failed logins
-                $login = "0"
-                While ($login -eq "0")
-                    {
-                        Try
-                            {
-                            
-                                Write-Host -ForegroundColor Yellow "Connecting to vCenter $vCenter!"
-                                Connect-VIServer $vCenter -user test -ErrorAction stop -WarningAction SilentlyContinue
-                                $login = "1"
-                            }
-
-                        Catch [VMware.VimAutomation.ViCore.Types.V1.ErrorHandling.InvalidLogin]
-                            {
-                                Write-Host -ForegroundColor Red "Wrong username or password. Please try again!"
-                                $login = "0"
-                            }
-                    }
+    (
+        [string]$vCenter
+    )
+    # Connecting to vCenter and check for failed logins
+    $login = "0"
+    While ($login -eq "0") {
+        Try {
+            Write-Host -ForegroundColor Yellow "Connecting to vCenter $vCenter!"
+            Connect-VIServer $vCenter -user test -ErrorAction stop -WarningAction SilentlyContinue
+            $login = "1"
+        }
+        Catch [VMware.VimAutomation.ViCore.Types.V1.ErrorHandling.InvalidLogin] {
+            Write-Host -ForegroundColor Red "Wrong username or password. Please try again!"
+            $login = "0"
+        }
+    }
 }
 
 
@@ -62,28 +53,25 @@ Connect2vCenter $vCenter
 $exportVMinfo = @()
 
 #Checking for all CI's of the virps in the array
-Foreach ($virp in $virps)
-    {
-        #Checking each Datastore that's linked to the VIRP for VM's and gathering all info.
-        $datastores = Get-Datastore | Where-Object {$_.Name -like "$virp*"}
-        foreach ($datastore in $datastores)
-            {
-                $vms = get-vm -Datastore $datastore
+Foreach ($virp in $virps) {
+    #Checking each Datastore that's linked to the VIRP for VM's and gathering all info.
+    $datastores = Get-Datastore | Where-Object { $_.Name -like "$virp*" }
+    foreach ($datastore in $datastores) {
+        $vms = get-vm -Datastore $datastore
                 
-                Foreach ($vm in $vms)
-                    {
-                        $VMSummary = "" | Select-Object Vmxpath, Name, Folder, Vlan, Datastore
-                        $VMSummary.Name = $vm.Name                
-                        $VMSummary.Folder = $vm.Folder
-                        $VMSummary.Vmxpath = $vm | Get-View | ForEach-Object {$_.Config.Files.VmPathName}
-			            $Networkname = $vm | Get-NetworkAdapter
-                        $VMSummary.Vlan = $Networkname.NetworkName
-                        $VMSummary.Datastore = $datastore
+        Foreach ($vm in $vms) {
+            $VMSummary = "" | Select-Object Vmxpath, Name, Folder, Vlan, Datastore
+            $VMSummary.Name = $vm.Name                
+            $VMSummary.Folder = $vm.Folder
+            $VMSummary.Vmxpath = $vm | Get-View | ForEach-Object { $_.Config.Files.VmPathName }
+            $Networkname = $vm | Get-NetworkAdapter
+            $VMSummary.Vlan = $Networkname.NetworkName
+            $VMSummary.Datastore = $datastore
                         
-                        $exportVMinfo += $VMSummary
-                    }
-            }
+            $exportVMinfo += $VMSummary
+        }
     }
+}
 
 Write-Host -ForegroundColor Yellow "Script is finished, all VM's can be found in $logfile"
 $exportVMinfo | Export-Csv $logfile -NoTypeInformation -Delimiter ";"
--- PowerCLI/Crelan DR/02_Unregister_VMs.ps1
+++ PowerCLI/Crelan DR/02_Unregister_VMs.ps1
@@ -9,74 +9,58 @@
    Another example of how to use this cmdlet
 #>
 
+# vCenter FQDN
+$vCenter = "hn1584.crelan.be"
 
+# CSV file name
+$vmfile = "DRP_vm_list.csv"
 
-Function Connection-vCenter
-{
-
+Function Connect2vCenter {
     Param 
-        (
-            [string]$Vcenter
-        )
-
-     #Connecting to vCenter Geleen with check for failed logins
-                $login = "0"
-                While ($login -eq "0")
-                    {
-                        Try
-                            {
+    (
+        [string]$vCenter
+    )
+    #Connecting to vCenter and check for failed logins
+    $login = "0"
+    While ($login -eq "0") {
+        Try {
                             
-                                Write-Host -ForegroundColor Yellow "Connecting to vCenter $vcenter!"
-                                Connect-VIServer $Vcenter -user test -ErrorAction stop -WarningAction SilentlyContinue
-                                $login = "1"
-                            }
-
-                        Catch [VMware.VimAutomation.ViCore.Types.V1.ErrorHandling.InvalidLogin]
-                            {
-                                Write-Host -ForegroundColor Red "Wrong username or password. Please try again!"
-                                $login = "0"
-                            }
-                    }
+            Write-Host -ForegroundColor Yellow "Connecting to vCenter $vCenter!"
+            Connect-VIServer $vCenter -user test -ErrorAction stop -WarningAction SilentlyContinue
+            $login = "1"
+        }
+        Catch [VMware.VimAutomation.ViCore.Types.V1.ErrorHandling.InvalidLogin] {
+            Write-Host -ForegroundColor Red "Wrong username or password. Please try again!"
+            $login = "0"
+        }
+    }
 }
 
 
 
-$vcenter = "hn1584.crelan.be"
-$vmfile = "DRP_vm_list.csv"
-
-
-
-
-# ask for confirmation before executing the script
-Write-host -ForegroundColor Yellow  "You are about the unregister the VM's from the file $vmfile" `n
+# Ask for confirmation before executing the script
+Write-Host -ForegroundColor Yellow  "You are about the unregister the VMs from the file $vmfile" `n
 $confirm = Read-Host "Are you sure you want to continue? Type [Yes] to confirm"
 
-if ($confirm -eq 'Yes')
-    {
-	    Connection-vCenter $vcenter
+if ($confirm -eq 'Yes') {
+    Connect2vCenter $vCenter
 
-        $csv = Import-csv  $vmfile -Delimiter ";"
+    $csv = Import-csv  $vmfile -Delimiter ";"
 
-        #Write-host "Showing input file $csv"
+    foreach ($row in $csv) { 
+        Write-host "Removing $($row.name) from datastore $($row.Datastore) in folder $($row.Folder)"
+        Remove-VM $row.Name -Confirm:$false        
+    } 
 
+    # Closing vCenter connection.
+    Disconnect-VIserver -confirm:$false  
+    
+    Write-Host -ForegroundColor Yellow "VMs have been unregistered based on file $vmfile."
 
+}
 
-        foreach ($row in $csv)
-	        { 
-	            Write-host "Removing $($vm.name) from $datastore in folder $($vmsummary.folder)"
-                Remove-VM $row.Name -Confirm:$false        
-   	        } 
-
-      
-        #disconnect from server
-        disconnect-viserver -confirm:$false
-        Write-Host -ForegroundColor Yellow "VM's have been unregistered based on the file $vmfile."
-
-    }
-
-Else
-    {
-        Write-host -ForegroundColor Red  "You have not typed Yes as answer but $confirm so the script will exit. Thank you for flying with us today."
-    }
+Else {
+    Write-host -ForegroundColor Red  "You have not typed Yes as answer but $confirm so the script will exit. Thank you for flying with us today."
+}
 
 
