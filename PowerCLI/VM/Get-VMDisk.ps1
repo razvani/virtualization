332f0c2986dd1410066201bc05baea9085ad3d9f
Jeroen Lambrichts <36880952+cgkjlambrichts@users.noreply.github.com>
2022-07-25 11:55:39 +0200

    Create Get-VMDisk.ps1

---
+++ PowerCLI/VM/Get-VMDisk.ps1
@@ -0,0 +1,32 @@
+function Get-VMDisk {
+
+[CmdletBinding()]
+    param( 
+        [Parameter(Mandatory=$True, ValueFromPipeline=$True, Position=0, HelpMessage = "VMs to process")]
+        [ValidateNotNullorEmpty()]
+          [VMware.VimAutomation.ViCore.Impl.V1.Inventory.InventoryItemImpl[]] $myVMs
+    )
+Process {
+        $View = @()
+        foreach ($myVM in $myVMs){
+            $VMDKs = $myVM | get-HardDisk
+            foreach ($VMDK in $VMDKs) {
+                if ($VMDK -ne $null){
+                    [int]$CapacityGB = $VMDK.CapacityKB/1024/1024
+                    $Report = [PSCustomObject] @{
+                            Name = $myVM.name 
+                            PowerState = $myVM.PowerState
+                            Datastore = $VMDK.FileName.Split(']')[0].TrimStart('[')
+                            VMDK = $VMDK.FileName.Split(']')[1].TrimStart('[')
+                            StorageFormat = $VMDK.StorageFormat
+                            CapacityGB = $CapacityGB
+                            Controller = $VMDK.ExtensionData.ControllerKey -1000
+                            Unit = $VMDK.ExtensionData.UnitNumber
+                        }
+                        $View += $Report
+                    }   
+                }
+            }
+    $View | Sort-Object VMname, Controller, Unit
+    }
+}
