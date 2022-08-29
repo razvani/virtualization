9bc272c83425958d514f7bc25227db4060976f16
Jeroen Lambrichts <36880952+jeroenlambrichts@users.noreply.github.com>
2022-08-29 15:31:14 +0200

    Update Change_ESXi_SNMP_community_string.ps1

--- PowerCLI/Host/Change_ESXi_SNMP_community_string.ps1
+++ PowerCLI/Host/Change_ESXi_SNMP_community_string.ps1
@@ -21,15 +21,14 @@ Write-Host -ForegroundColor Yellow "Importing CSV file $DataFile"
 $ESXiHosts = Import-Csv -Path $DataFile -Delimiter $CSVfileDelimiter
 Write-Host ($ESXiHosts | Measure-Object).count "ESXi hosts to process!"
 
-
-# $ESXiHostSnmp_String  = Read-Host -Prompt "`r`nInput the SNMP community string" # doesn't work this way. use: -ReadOnlyCommunity public, cegekamonsecured, ihatesnmp, ireallyhatesnmp
-
 $ESXiCredentials = Get-Credential -Message "Please enter the ESXi host account credentials"
 
 ForEach ($ESXiHost in $ESXiHosts){
 	Write-Host -ForegroundColor Yellow "`r`nConnecting to" $ESXiHost.Name
-    Connect-VIServer $ESXiHost.Name -Credential $ESXiCredentials
-	Get-VMHostSnmp | Set-VMHostSnmp -Enabled:$True -ReadOnlyCommunity pulic, cegekamonsecured 
-    #Get-VMHostSnmp | Select ReadOnlyCommunities, Enabled
+   	Connect-VIServer $ESXiHost.Name -Credential $ESXiCredentials
+	Get-VMHostSnmp | Set-VMHostSnmp -Enabled:$True -ReadOnlyCommunity firststring, secondstring #change first- and secondstring to public, and check in PIM
+    	
+	#Get-VMHostSnmp | Select ReadOnlyCommunities, Enabled
 	Disconnect-VIServer -Server * -Force -Confirm:$False
+	
 }
