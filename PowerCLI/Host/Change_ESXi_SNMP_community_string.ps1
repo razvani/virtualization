9a84285f0852b84c4c934f37594509c8b3ce9ef5
Razvan Irimia <razvan.irimia@cegeka.com>
2022-09-07 11:29:33 +0300

    SNMP strings as variables

--- PowerCLI/Host/Change_ESXi_SNMP_community_string.ps1
+++ PowerCLI/Host/Change_ESXi_SNMP_community_string.ps1
@@ -1,9 +1,16 @@
 #################################################################################
 # This script is created for configuring SNMP on multiple ESXi hosts
-# Parameters
-$CSVfile = 'ESXiHosts.csv' #Default name for CSV input file
+# Parameters:
+#
+# CSV file
+$CSVfile = 'ESXiHosts.csv' #Default name for CSV input file ## CSV header: Name
 $CSVfileDelimiter = ','
-# CSV header:    Name
+#
+# Community strings
+$snmpString1 = "public"
+$twoStrings = True       #Set to 'False' if you want to set just one string
+$snmpString2 = "secondstring"  #Secure string (should be in PIM)
+#
 #################################################################################
 
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
@@ -21,14 +28,19 @@ Write-Host -ForegroundColor Yellow "Importing CSV file $DataFile"
 $ESXiHosts = Import-Csv -Path $DataFile -Delimiter $CSVfileDelimiter
 Write-Host ($ESXiHosts | Measure-Object).count "ESXi hosts to process!"
 
-$ESXiCredentials = Get-Credential -Message "Please enter the ESXi host account credentials"
+$ESXiCredentials = Get-Credential -Message "Please enter the ESXi host account credentials."
 
+# Setting the SNMP community string on each host.
 ForEach ($ESXiHost in $ESXiHosts){
 	Write-Host -ForegroundColor Yellow "`r`nConnecting to" $ESXiHost.Name
    	Connect-VIServer $ESXiHost.Name -Credential $ESXiCredentials
-	Get-VMHostSnmp | Set-VMHostSnmp -Enabled:$True -ReadOnlyCommunity firststring, secondstring #change first- and secondstring to public, and check in PIM
-    	
+	
+	if ($twoStrings) {
+		Get-VMHostSnmp | Set-VMHostSnmp -Enabled:$True -ReadOnlyCommunity $snmpString1, $snmpString2
+	} else {
+		Get-VMHostSnmp | Set-VMHostSnmp -Enabled:$True -ReadOnlyCommunity $snmpString1
+	}
+	
 	#Get-VMHostSnmp | Select ReadOnlyCommunities, Enabled
 	Disconnect-VIServer -Server * -Force -Confirm:$False
-	
 }
