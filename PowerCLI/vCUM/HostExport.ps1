9ce58c0c36225eed958e32cbaa4afb574d0d19fb
Jeroen Lambrichts <36880952+jeroenlambrichts@users.noreply.github.com>
2023-01-04 10:53:46 +0100

    Create HostExport.ps1

---
+++ PowerCLI/vCUM/HostExport.ps1
@@ -0,0 +1,72 @@
+# Connect to vCenter
+try {
+  Connect-VIServer sharbehavcsa101.cegekavirtual.local -Credential (Get-Credential) -Alllinked
+}
+catch {
+  Write-Error "Error connecting to vCenter: $_"
+  exit
+}
+
+# Get the current date and time
+$date = Get-Date
+
+# Format the date and time in the desired format
+$formattedDate = $date.ToString("yyyyMMddHHmm")
+
+# Build the filename using the formatted date
+$filename = "$formattedDate-HostExport.csv"
+
+# Get a list of all ESXi hosts with SHAR in their name
+try {
+  $hosts = Get-VMHost | Where-Object { $_.Name -like "*SHAR*" }
+}
+catch {
+  Write-Error "Error getting list of hosts: $_"
+  exit
+}
+
+# Initialize an array to store the host information
+$hostInfo = @()
+
+# For each host, set the values of the properties and add them to the array
+foreach ($VMHost in $hosts) {
+  # Determine the value of the new property
+  switch -wildcard ($VMHost.Parent) {
+    "*DR*" { $os = "" }
+    "*CLUW*" { $os = "MICROSOFT" }
+    "*CLUL*" { $os = "LINUX" }
+    default { $os = "" }
+  }
+
+  # Determine the value of the datacenter property
+  switch -wildcard ($VMHost.Name) {
+    "*BEHAESXDR*" { $datacenter = "HASSELT-T3" }
+    "*NLGEESXDR*" { $datacenter = "GELEEN-T3" }
+    "*NLGEESX*" { $datacenter = "GELEEN" }
+    "*BEHAESX*" { $datacenter = "HASSELT" }
+    default { $datacenter = "" }
+  }
+
+  # Add the host's name, memory, number of CPUs, new property value, and parent cluster to the array
+  # only if it is not excluded
+  if (!($VMHost.Name -like "*upl*") -and !($VMHost.Name -like "*migtoaci*") -and !($VMHost.Parent -like "*reinstall*") -and !($VMHost.Parent -like "*TEST*")) {
+    $hostInfo += [PSCustomObject]@{
+      Name = $VMHost.Name
+      NumCPU = $VMHost.NumCpu
+      MemoryGB = [Math]::Round($VMHost.MemoryTotalGB)
+      OS = $os
+      Datacenter = $datacenter
+      Cluster = $VMHost.Parent
+    }
+  }
+}
+
+# Export the array to a CSV file with the formatted date in the filename
+try {
+  $hostInfo | Export-Csv -Path "C:\Temp\$filename" -NoTypeInformation
+}
+catch {
+  Write-Error "Error exporting to CSV file: $_"
+}
+
+Disconnect-VIServer * -Confirm:$false
